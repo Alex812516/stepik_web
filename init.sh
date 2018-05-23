cb08ac7989cad81a5ff68938f0e60515d837e3f2
Alex812516 <39481029+Alex812516@users.noreply.github.com>
2018-05-23 10:50:38 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
-git config --global user.name Alex812516
-git config --global user.email bae71@yandex.ru
 
   sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
   sudo /etc/init.d/nginx restart
